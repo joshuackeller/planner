6881f4c99394e60fb70acfa1474994d074d1c60e
joshuackeller <joshuackeller@gmail.com>
2024-12-07 13:33:48 -0700

    remove secrets

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* <script src="https://unpkg.com/react-scan/dist/auto.global.js"></script> */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
